2572011978a20d9d0c7b2492d586930a19cb20cf
Maneth Perera <manethb2003@gmail.com>
2026-05-03 16:11:09 +0530

    Remove unused import for ClassifierBaselineNetwork

--- src/coco_tasks/single_task_experiments.py
+++ src/coco_tasks/single_task_experiments.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 
 from coco_tasks.settings import TB_ROOT
 from coco_tasks.single_task_datasets import CocoTasksTest, CocoTasksGT, CocoTasksTestGT
-from coco_tasks.single_task_networks import ClassifierBaselineNetwork
+
 
 
 class OverfitSampler(Sampler):
